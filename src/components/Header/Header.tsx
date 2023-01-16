9fccc0b7f1b3756ce15777eb875dd1e1b1774f6c
UrielBm <urielbenitez96@hotmail.com>
2023-01-16 10:30:22 -0600

    feature: header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import Logo from "./../../assets/logo.svg";
+const Header = () => {
+  return (
+    <header className="header">
+      <Link to="/">
+        <img src={Logo} className="logo" />
+      </Link>
+      <nav className="nav">
+        <Link to="/about" className="items">
+          Nosotros
+        </Link>
+        <Link to="/scores" className="items">
+          Puntajes
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
