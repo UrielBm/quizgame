386041edf5a29d91b6226be5bb05660c58fdeff8
UrielBm <urielbenitez96@hotmail.com>
2023-01-16 17:27:06 -0600

    hotfix add width and height to logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,13 @@ const Header = () => {
   return (
     <header className="header">
       <Link to="/">
-        <img src={Logo} className="logo" alt="logo" />
+        <img
+          src={Logo}
+          className="logo"
+          alt="logo"
+          width="96px"
+          height="96px"
+        />
       </Link>
       <nav className="nav">
         <Link to="/about" className="items">
