a3fe6cad4787ed6fb628ad4d4174402a57ea067c
UrielBm <urielbenitez96@hotmail.com>
2023-01-16 10:32:34 -0600

    feature: new modal component for the alerts

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,33 @@
+import Lottie from "lottie-react";
+import ErrorLuttie from "./../../assets/lotties/error.json";
+import WinnerLuttie from "./../../assets/lotties/winner.json";
+import Logo from "./../../assets/logo.svg";
+import { useNavigate } from "react-router-dom";
+import { myModalInterface } from "../../config/interfaces";
+
+const Modal = ({ text, statusCode }: myModalInterface) => {
+  const navigate = useNavigate();
+  return (
+    <div className="modal">
+      <div className="modal__wrapper">
+        <div className="modal__wrapperButton">
+          <button className="modal__closeButton" onClick={() => navigate("/")}>
+            X
+          </button>
+        </div>
+        <img src={Logo} className="modalLogo" />
+        <div>
+          <p>{text}</p>
+          <div>
+            <Lottie
+              animationData={statusCode === 200 ? WinnerLuttie : ErrorLuttie}
+              loop={true}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
