aea8a51f6cc3caae2a006a18725bec18f72ed85e
UrielBm <urielbenitez96@hotmail.com>
2023-01-16 17:33:37 -0600

    hotfix: add width and height

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <header className="header">
       <Link to="/">
-        <img src={Logo} className="logo" alt="logo" />
+        <img src={Logo} className="logo" alt="logo" width={96} height={96} />
       </Link>
       <nav className="nav">
         <Link to="/about" className="items">
