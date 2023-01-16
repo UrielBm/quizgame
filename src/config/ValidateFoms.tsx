9fecbc79e853e7845403b68438a3038560cd0ad6
UrielBm <urielbenitez96@hotmail.com>
2023-01-16 17:50:08 -0600

    hotfix: add validations to RegisterForm

--- src/config/ValidateFoms.tsx
+++ src/config/ValidateFoms.tsx
@@ -4,7 +4,10 @@ export const CategorySchema = Yup.object().shape({
   category: Yup.string().required("Es necesario seleccionar una categoria"),
 });
 export const GamertagSchema = Yup.object().shape({
-  gamertag: Yup.string().required("Es necesario un nombre de jugador"),
+  gamertag: Yup.string()
+    .required("Es necesario un nombre de jugador")
+    .min(3, "Tu gamertag debe tener al menos 3 letras")
+    .max(7, "Tu gamertag debe ser máximo de 7 letras"),
 });
 
 export const CategoryValue: interfaceCategories = { category: "" };
