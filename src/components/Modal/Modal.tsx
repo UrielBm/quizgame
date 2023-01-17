6c8d1ca5c544a5fc9c4b9467718ac190e6692d4f
UrielBm <urielbenitez96@hotmail.com>
2023-01-17 17:07:31 -0600

    styles and link to scores

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,22 +2,26 @@ import Lottie from "lottie-react";
 import ErrorLuttie from "./../../assets/lotties/error.json";
 import WinnerLuttie from "./../../assets/lotties/winner.json";
 import Logo from "./../../assets/logo.svg";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { myModalInterface } from "../../config/interfaces";
 
 const Modal = ({ text, statusCode }: myModalInterface) => {
-  const navigate = useNavigate();
   return (
     <div className="modal">
       <div className="modal__wrapper">
         <div className="modal__wrapperButton">
-          <button className="modal__closeButton" onClick={() => navigate("/")}>
+          <Link className="modal__closeButton" to="/">
             X
-          </button>
+          </Link>
         </div>
         <img src={Logo} className="modalLogo" alt="logo" />
         <div>
           <p>{text}</p>
+          {statusCode === 200 && (
+            <Link to="/scores" className="modal__scores">
+              Puntajes
+            </Link>
+          )}
           <div className="modal__wrapperLottie">
             <Lottie
               animationData={statusCode === 200 ? WinnerLuttie : ErrorLuttie}
