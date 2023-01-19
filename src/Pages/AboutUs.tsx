7cf851759762931621542b94c3b54a63a6f8c84b
UrielBm <urielbenitez96@hotmail.com>
2023-01-19 17:10:43 -0600

    hotfix: text error in aboutUs

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -7,14 +7,14 @@ const AboutUs = () => {
       <div className="wrapperAboutUs">
         <p>
           Acerca del proyecto de encuestados: este fue creado como proyecto
-          final para probar, diferentes conceptos y tecnologias para el bootcamp
+          final para probar, diferentes conceptos y tecnologías para el bootcamp
           de Código facilito Front-End.
         </p>
         <p>
-          Consiste el proyecto de un juego tipo "Quiz" por categorias donde cada
-          categoria contiene diferentes preguntas, mientras más aciertes mejor
-          sera tu puntaje el cual puedes registrar y visualizar el top 5 mejores
-          puntajes según la categoria.
+          Consiste el proyecto de un juego tipo "Quiz" por categorías donde cada
+          categoría contiene diferentes preguntas, mientras más aciertes mejor
+          será tu puntaje el cual puedes registrar y visualizar el top 5 mejores
+          puntajes según la categoría.
         </p>
         <p>
           Pruebate a ti mismo y a tus amigos para conseguir el mejor puntaje.
