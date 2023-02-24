f6ba8c15b2e7a2a78d6dd6f2aa5771991574e7a1
UrielBm <urielbenitez96@hotmail.com>
2023-02-24 12:53:05 -0600

    edit BrowerRouter to HasRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import QuizState from "./context/QuizState";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <QuizState>
         <App />
       </QuizState>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
