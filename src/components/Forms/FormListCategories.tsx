7e15384b038affe43a52ed71a3f602eb398e1c6e
UrielBm <urielbenitez96@hotmail.com>
2023-02-24 12:14:07 -0600

    loading status when looking categories

--- src/components/Forms/FormListCategories.tsx
+++ src/components/Forms/FormListCategories.tsx
@@ -7,12 +7,13 @@ import { Formik, Field, Form, ErrorMessage } from "formik";
 import { CategorySchema, CategoryValue } from "../../config/ValidateFoms";
 import { category, ModalInterface } from "../../config/interfaces";
 import "animate.css";
+import Spinner from "../Spinner/Spinner";
 
 const FormListCategories = () => {
   const AppContext = useContext(QuizContext);
   if (!AppContext) return null;
   const { handleGetQuizByCategory } = AppContext;
-  const { list } = useGetCategories();
+  const { list, isLoading } = useGetCategories();
   const [modalStatus, setmodalStatus] = useState<ModalInterface>({
     isOpen: false,
     text: "",
@@ -32,15 +33,26 @@ const FormListCategories = () => {
           <Form className="animate__animated animate__zoomIn form">
             <Title title="Categorias" />
             <div role="group" className="wrapperCategories">
-              {list.map(({ _id: id, name }: category) => (
-                <label
-                  key={id}
-                  className={values.category === id ? "labelChecked" : "label"}
-                >
-                  <Field type="radio" name="category" value={id} />
-                  {name}
-                </label>
-              ))}
+              {isLoading ? (
+                <Spinner />
+              ) : list.length !== 0 ? (
+                list.map(({ _id: id, name }: category) => (
+                  <label
+                    key={id}
+                    className={
+                      values.category === id ? "labelChecked" : "label"
+                    }
+                  >
+                    <Field type="radio" name="category" value={id} />
+                    {name}
+                  </label>
+                ))
+              ) : (
+                <p className="no-conectionMessage">
+                  Upps... no se pudo recuperar las categorias :/, intenta
+                  conectarte despues.
+                </p>
+              )}
             </div>
             <p className="error">
               <ErrorMessage name="category" />
