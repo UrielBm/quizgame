7ade8e2329ce8cebd6d494eba2581d9435702eaa
UrielBm <urielbenitez96@hotmail.com>
2023-02-24 12:15:16 -0600

    status error when no looking the categories

--- src/hooks/useGetCategories.tsx
+++ src/hooks/useGetCategories.tsx
@@ -4,18 +4,23 @@ import clienteAxios from "../config/axios";
 
 const useGetCategories = () => {
   const [list, setList] = useState<category[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   useEffect(() => {
     handleGetAllCategories();
   }, []);
   const handleGetAllCategories = async () => {
     try {
+      setIsLoading(true);
       const { data } = await clienteAxios.get("categories");
+      setIsLoading(false);
       setList(data);
     } catch (error) {
-      console.log(error);
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
     }
   };
-  return { list };
+  return { list, isLoading };
 };
 
 export default useGetCategories;
